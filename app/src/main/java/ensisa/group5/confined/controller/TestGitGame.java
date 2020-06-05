a45a6b346f366a115c70c86b504a1f089a742c2e
OiseauPerchey <marcetudie@gmail.com>
2020-06-05 19:43:06 +0200

    ajout de l'activité vide scoreboard.java et scoreBoard.xml

--- app/src/main/java/ensisa/group5/confined/controller/TestGitGame.java
+++
@@ -1,8 +0,0 @@
-package ensisa.group5.confined.controller;
-
-public class TestGitGame {
-    public TestGitGame() {
-        System.out.println("Ceci est juste un test git");
-    }
-
-}
---
+++ app/src/main/java/ensisa/group5/game/ScoreBoardActivity.java
@@ -0,0 +1,14 @@
+package ensisa.group5.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScoreBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score_board);
+    }
+}
