a35fd67ec73621d87648f3e52ae2c6ce9408ece3
zeinebzelleg <zaineb.zelleg@gmail.com>
2020-06-06 19:28:38 +0100

    adding StepCounter class

--- app/src/main/java/ensisa/group5/confined/controller/MainActivity.java
+++ app/src/main/java/ensisa/group5/confined/controller/MainActivity.java
@@ -2,7 +2,12 @@ package ensisa.group5.confined.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,6 +17,8 @@ import android.widget.EditText;
 <<<<<<< HEAD
 =======
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.android.gms.tasks.Continuation;
@@ -38,7 +45,7 @@ import java.util.List;
 import ensisa.group5.confined.R;
 import ensisa.group5.confined.exceptions.DataBaseException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private EditText usernameEdit;
     private EditText passwordEdit;
@@ -183,6 +190,36 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor!=null){
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Sensor not found ! ", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        running = false;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(running){
+            tv_steps.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
 
--- app/src/main/java/ensisa/group5/confined/model/Task.java
+++ app/src/main/java/ensisa/group5/confined/model/Task.java
@@ -1,13 +1,33 @@
 package ensisa.group5.confined.model;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+
 /**
  * Author VALLERICH Vincent on 05-06-2020
  */
 
-public class Task {
+public class Task implements SensorEventListener {
     private int id;
     private String name;
     private String description;
+
+    TextView tv_steps;
+    SensorManager sensorManager;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
     private enum Priority {MINOR, IMPORTANT, URGENT};
     private int points;
 
