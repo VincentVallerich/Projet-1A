69f0b7dc94e7b416e992a5c8526c7ddee720db4a
VallerichVincent <vallerichvincent@outlook.fr>
2020-06-09 23:25:37 +0200

    Revert "Add setPseudo"
    This reverts commit 103e916a912fee30d3cdccd46dbf51ad03070d51.

--- app/src/main/java/ensisa/group5/confined/controller/DataBase.java
+++ app/src/main/java/ensisa/group5/confined/controller/DataBase.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
-import androidx.annotation.Nullable;
-
 import com.google.android.gms.tasks.Task;
 import com.mongodb.stitch.android.core.Stitch;
 import com.mongodb.stitch.android.core.StitchAppClient;
@@ -24,8 +22,6 @@ import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -94,7 +90,7 @@ public class DataBase implements Executor {
      *Retourne un task qui contient une liste de documents
      * Les Documents contiennent les JSON des informations des tâches non assignées ( task_status = 0 ). Utile pour que les utilisateurs puissent les choisir
      */
-    public RemoteFindIterable<Document> getNonAssignedTasks() {
+    public RemoteFindIterable<Document>  getNonAssignedTasks() {
         try {
             RemoteMongoClient remoteMongoClient = Stitch.getDefaultAppClient().getServiceClient(RemoteMongoClient.factory, serviceName);
             RemoteMongoCollection<Document> collection = remoteMongoClient.getDatabase(databaseName).getCollection(collectionNameTasks);
@@ -122,7 +118,7 @@ public class DataBase implements Executor {
      *Retourne un task qui contient un document
      * Le Document contient le JSON des informations de l'utilisateur connecté à l'application
      */
-    public Task<Document> getUserInfo() {
+   /* public Task<Document> getUserInfo() {
         try {
             final RemoteMongoClient remoteMongoClient = Stitch.getDefaultAppClient().getServiceClient(RemoteMongoClient.factory, "Mongo-Confined");
             RemoteMongoCollection<Document> collection = remoteMongoClient.getDatabase(databaseName).getCollection("Users_data");
@@ -137,7 +133,9 @@ public class DataBase implements Executor {
         return null;
     }
 
-    public Task<RemoteUpdateResult> startTask(String taskid){
+    */
+
+    public  Task<RemoteUpdateResult> startTask(String taskid){
         RemoteMongoClient remoteMongoClient = Stitch.getDefaultAppClient().getServiceClient(RemoteMongoClient.factory, serviceName);
         RemoteMongoCollection<Document> collection = remoteMongoClient.getDatabase(databaseName).getCollection(collectionNameTasks);
         final Document filterDoc = new Document( "_id", new ObjectId(taskid));
@@ -146,8 +144,6 @@ public class DataBase implements Executor {
 
         return collection.updateOne(filterDoc, updateDoc);
     }
-
-    //public
 
     /**
      *
@@ -163,15 +159,8 @@ public class DataBase implements Executor {
                 .append(field_task_priority, task.getPriority().toString())
                 .append(field_task_description, task.getDescription())
                 .append(field_task_score, String.valueOf(task.getPoints()));
-        return collection.insertOne(newTask);
-    }
 
-    public void setPseudo(String id_user, String pseudo) {
-        RemoteMongoClient remoteMongoClient = Stitch.getDefaultAppClient().getServiceClient(RemoteMongoClient.factory, serviceName);
-        RemoteMongoCollection<Document> collection = remoteMongoClient.getDatabase(databaseName).getCollection(collectionNameUsersData);
-        final Document filterDoc = new Document( "_id", new ObjectId(id_user));
-        Document updateDoc = new Document().append("$set",new Document().append("pseudo", pseudo));
-        collection.updateOne(filterDoc, updateDoc);
+        return collection.insertOne(newTask);
     }
 
     /**
@@ -195,10 +184,9 @@ public class DataBase implements Executor {
         RemoteMongoClient remoteMongoClient = Stitch.getDefaultAppClient().getServiceClient(RemoteMongoClient.factory, serviceName);
         RemoteMongoCollection<Document> collection = remoteMongoClient.getDatabase(databaseName).getCollection(collectionNameTasks);
         final Document filterDoc = new Document( "_id", new ObjectId(taskid));
-        Document updateDoc = new Document().append("$set",new Document().append(field_task_status, CTask.State.NON_ATTRIBUATE.toString())).append("user_id","");
+        Document updateDoc = new Document().append("$set",new Document().append(field_task_status, CTask.State.NON_ATTRIBUATE.toString())).append("user_id","");;
         return collection.updateOne(filterDoc, updateDoc);
     }
-
     public Task<RemoteDeleteResult> deleteTask(String taskid){
         RemoteMongoClient remoteMongoClient = Stitch.getDefaultAppClient().getServiceClient(RemoteMongoClient.factory, serviceName);
         RemoteMongoCollection<Document> collection = remoteMongoClient.getDatabase(databaseName).getCollection(collectionNameTasks);
