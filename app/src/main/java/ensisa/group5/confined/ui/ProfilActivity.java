3d277c8bf958bda7277e1e44ffefc3227d997fd0
AmalJedusor <amelpoukoull@gmail.com>
2020-06-11 22:21:07 +0200

    fin du projet ?

--- app/src/main/java/ensisa/group5/confined/ui/ProfilActivity.java
+++ app/src/main/java/ensisa/group5/confined/ui/ProfilActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -108,10 +109,10 @@ public class ProfilActivity extends AppCompatActivity implements View.OnClickLis
                     modifyProfilPopup.dismiss();
                     break;
                 case R.id.logout_button:
-                    preferences.edit().clear().apply();
-                    Intent intent5 = new Intent(this, MainActivity.class);
-                    startActivity(intent5);
+                    SharedPreferences preferences = getSharedPreferences( getString(R.string.PREF_KEY_MAIL), MODE_PRIVATE);
+                    preferences.edit().clear().commit();
                     finish();
+                    System.exit(0);
                     break;
             }
         }
